2bbfdace2eb856f72e02a091c562856ce8fc6c90
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-06 17:06:10 -0700

    add install scripts

---
+++ scripts/install.ps1
@@ -0,0 +1,43 @@
+$version = "0.1.0"
+$url = "https://bacongobbler.blob.core.windows.net/gofish/fish-v$version-windows-amd64.zip"
+
+if ($env:TEMP -eq $null) {
+  $env:TEMP = Join-Path $env:SystemDrive 'temp'
+}
+$file = Join-Path $env:TEMP "fish-v$version-windows-amd64.zip"
+
+function Download-String {
+param (
+  [string]$url
+ )
+  $downloader = Get-Downloader $url
+
+  return $downloader.DownloadString($url)
+}
+
+function Download-File {
+param (
+  [string]$url,
+  [string]$file
+ )
+
+  $downloader = new-object System.Net.WebClient
+
+  $downloader.DownloadFile($url, $file)
+}
+
+# Download fish
+Write-Output "Downloading $url"
+Download-File $url $file
+
+Write-Output "Preparing to install into $installPath"
+$installPath = "$env:SYSTEMDRIVE\ProgramData\bin"
+Expand-Archive -Path "$file" -DestinationPath "$tempDir" -Force
+
+Write-Output "fish installed into $installPath\fish"
+Write-Output "Run 'fish init' to get started!"
+
+# Add fish to the path
+if ($($env:Path).ToLower().Contains($($installPath).ToLower()) -eq $false) {
+  $env:Path = [Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine);
+}
