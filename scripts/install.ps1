b104dd2de32299e5d7970fd39c6651df1c964789
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-06 21:30:22 -0700

    fixup

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -4,39 +4,26 @@ $url = "https://bacongobbler.blob.core.windows.net/gofish/fish-v$version-windows
 if ($env:TEMP -eq $null) {
   $env:TEMP = Join-Path $env:SystemDrive 'temp'
 }
+$tempDir = Join-Path $env:TEMP 'Fish'
+if (![System.IO.Directory]::Exists($tempDir)) {[void][System.IO.Directory]::CreateDirectory($tempDir)}
 $file = Join-Path $env:TEMP "fish-v$version-windows-amd64.zip"
-
-function Download-String {
-param (
-  [string]$url
- )
-  $downloader = Get-Downloader $url
-
-  return $downloader.DownloadString($url)
-}
-
-function Download-File {
-param (
-  [string]$url,
-  [string]$file
- )
-
-  $downloader = new-object System.Net.WebClient
-  $downloader.DownloadFile($url, $file)
-}
 
 # Download fish
 Write-Output "Downloading $url"
-Download-File $url $file
+(new-object System.Net.WebClient).DownloadFile($url, $file)
 
-Write-Output "Preparing to install into $installPath"
 $installPath = "$env:SYSTEMDRIVE\ProgramData\bin"
-Expand-Archive -Path "$file" -DestinationPath "$installPath" -Force
+if (![System.IO.Directory]::Exists($installPath)) {[void][System.IO.Directory]::CreateDirectory($installPath)}
+Write-Output "Preparing to install into $installPath"
 
-Write-Output "fish installed into $installPath\fish"
-Write-Output "Run 'fish init' to get started!"
+Expand-Archive -Path "$file" -DestinationPath "$tempDir" -Force
+Move-Item -Path "$tempDir\windows-amd64\fish.exe" -Destination "$installPath\fish.exe"
+
+Write-Output "fish installed into $installPath\fish.exe"
+Write-Output "Restart your terminal, then run 'fish init' to get started!"
 
 # Add fish to the path
 if ($($env:Path).ToLower().Contains($($installPath).ToLower()) -eq $false) {
-  $env:Path = [Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine);
+  $newPath = [Environment]::GetEnvironmentVariable('Path',[System.EnvironmentVariableTarget]::Machine) + ";$installPath";
+  [Environment]::SetEnvironmentVariable('Path',$newPath,[System.EnvironmentVariableTarget]::Machine);
 }
