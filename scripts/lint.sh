20b88630532666e13c89b29b8d3457a186ba5e1c
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-09 20:52:53 -0700

    fix gometalinter calls

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,7 @@ fi
 
 # Mandatory tests
 echo -e "\033[0;31mManadatory Linters: These must pass\033[0m"
-gometalinter --vendor --tests --deadline=20s --disable-all \
+gometalinter.v1 --vendor --tests --deadline=20s --disable-all \
 --enable=gofmt \
 --enable=misspell \
 --enable=deadcode \
@@ -19,7 +19,7 @@ mandatory=$?
 
 # Optional tests
 echo -e "\033[0;32mOptional Linters: These should pass\033[0m"
-gometalinter --vendor --tests --deadline=20s --disable-all \
+gometalinter.v1 --vendor --tests --deadline=20s --disable-all \
 --enable=golint \
 ./...
 
