39875501a6956402023df4170987949b3a971851
Matthew Fisher <matt.fisher@microsoft.com>
2018-04-20 22:50:18 -0700

    implement `gofish rotten`
    and this time for real!

--- cmd/gofish/rotten.go
+++ cmd/gofish/rotten.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 
+	"github.com/fishworks/gofish"
+	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +13,24 @@ func newRottenCmd() *cobra.Command {
 		Use:   "rotten",
 		Short: "show fish food past their best before date (outdated)",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return errors.New("`gofish rotten` is not implemented")
+			table := uitable.New()
+			table.AddRow("NAME", "VERSION")
+			for _, name := range findFood() {
+				versions := findFoodVersions(name)
+				if len(versions) > 1 {
+					for _, ver := range versions {
+						f := gofish.Food{
+							Name:    name,
+							Version: ver,
+						}
+						if !f.Linked() {
+							table.AddRow(f.Name, f.Version)
+						}
+					}
+				}
+			}
+			fmt.Println(table)
+			return nil
 		},
 	}
 	return cmd
