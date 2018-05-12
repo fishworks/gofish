8fa29d807b1a69ea1ce8163ae80e7f38b2782cff
Matthew Fisher <matt.fisher@microsoft.com>
2018-05-12 11:46:07 +0100

    fix issue parsing repo name from search results

--- cmd/gofish/search.go
+++ cmd/gofish/search.go
@@ -71,8 +71,8 @@ func findFishFood() []string {
 		}
 		if !f.IsDir() && strings.HasSuffix(f.Name(), ".lua") {
 			foodName := strings.TrimSuffix(f.Name(), ".lua")
-			repoName := strings.TrimPrefix(p, rigPath+"/")
-			repoName = strings.TrimSuffix(repoName, "/"+path.Join("Food", f.Name()))
+			repoName := strings.TrimPrefix(p, rigPath+string(os.PathSeparator))
+			repoName = strings.TrimSuffix(repoName, string(os.PathSeparator)+filepath.Join("Food", f.Name()))
 			name := foodName
 			if repoName != home.DefaultRig() {
 				name = path.Join(repoName, foodName)
