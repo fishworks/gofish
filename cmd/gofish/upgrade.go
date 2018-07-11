9f8a2b53803580a5183f5d399b9f6f3622d5977e
Matthew Fisher <matt.fisher@microsoft.com>
2018-07-11 08:42:13 -0700

    only upgrade packages that need upgrading

--- cmd/gofish/upgrade.go
+++ cmd/gofish/upgrade.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/Masterminds/semver"
 	"github.com/fishworks/gofish/pkg/ohai"
 	"github.com/spf13/cobra"
 )
@@ -21,11 +23,27 @@ func newUpgradeCmd() *cobra.Command {
 			} else {
 				foodNames = findFood()
 			}
+			nothingUpgraded := true
 			for _, name := range foodNames {
+				installedVersions := findFoodVersions(name)
+				vs := make(semver.Collection, len(installedVersions))
+				for i, r := range installedVersions {
+					v, err := semver.NewVersion(r)
+					if err != nil {
+						return fmt.Errorf("Error parsing version: %v", err)
+					}
+					vs[i] = v
+				}
+				// we can safely assume there's at least one release installed
+				latestInstalledVersion := vs[len(vs)-1]
 				food, _, err := getFood(name)
 				if err != nil {
 					return err
 				}
+				if latestInstalledVersion.String() == food.Version {
+					continue
+				}
+				nothingUpgraded = false
 				ohai.Ohaif("Upgrading %s...\n", food.Name)
 				start := time.Now()
 				if err := food.Install(); err != nil {
@@ -33,6 +51,9 @@ func newUpgradeCmd() *cobra.Command {
 				}
 				t := time.Now()
 				ohai.Successf("%s %s: upgraded in %s\n", food.Name, food.Version, t.Sub(start).String())
+			}
+			if nothingUpgraded {
+				ohai.Successf("Everything up to date!\n")
 			}
 			return nil
 		},
