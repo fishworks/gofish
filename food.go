cfad0b64e6c0b3f09ad32f761c4fc48392b2e99a
SeekingMeaning <meaningseeking@protonmail.com>
2020-01-21 09:52:58 -0800

    Update food.go

--- food.go
+++ food.go
@@ -175,14 +175,7 @@ func (f *Food) Uninstall() error {
 }
 
 func unarchiveOrCopy(src, dest, urlPath string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	err = archiver.Unarchive(src, dest)
-	if err == nil {
+	if err := archiver.Unarchive(src, dest); err == nil {
 		return nil
 	}
 	out, err := os.Create(filepath.Join(dest, filepath.Base(urlPath)))
