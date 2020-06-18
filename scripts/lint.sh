4bd81958d9247124dedea387a8d28f066130fa5b
Matthew Fisher <matt.fisher@microsoft.com>
2020-06-18 16:03:09 -0700

    golangci-lint 1.27.0
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! hash golangci-lint 2>/dev/null ; then
-  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.24.0
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.27.0
 fi
 
 # mandatory tests
