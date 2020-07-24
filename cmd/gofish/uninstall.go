a08113d3e070499b76dfbe54d45b1dda3c86c524
Matthew Fisher <matt.fisher@microsoft.com>
2020-07-24 09:37:20 -0700

    accept multiple arguments for `gofish uninstall`
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- cmd/gofish/uninstall.go
+++ cmd/gofish/uninstall.go
@@ -14,39 +14,40 @@ func newUninstallCmd() *cobra.Command {
 		Use:     "uninstall <food>",
 		Aliases: []string{"rm", "remove"},
 		Short:   "uninstall fish food",
-		Args:    cobra.ExactArgs(1),
+		Args:    cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fishFood := args[0]
-			relevantFood := search([]string{fishFood})
-			switch len(relevantFood) {
-			case 0:
-				return fmt.Errorf("no fish food with the name '%s' was found", fishFood)
-			case 1:
-				fishFood = relevantFood[0]
-			default:
-				var match bool
-				// check if we have an exact match
-				for _, f := range relevantFood {
-					if strings.Compare(f, fishFood) == 0 {
-						fishFood = f
-						match = true
+			for _, fishFood := range args {
+				relevantFood := search([]string{fishFood})
+				switch len(relevantFood) {
+				case 0:
+					return fmt.Errorf("no fish food with the name '%s' was found", fishFood)
+				case 1:
+					fishFood = relevantFood[0]
+				default:
+					var match bool
+					// check if we have an exact match
+					for _, f := range relevantFood {
+						if strings.Compare(f, fishFood) == 0 {
+							fishFood = f
+							match = true
+						}
+					}
+					if !match {
+						return fmt.Errorf("%d fish food with the name '%s' was found: %v", len(relevantFood), fishFood, relevantFood)
 					}
 				}
-				if !match {
-					return fmt.Errorf("%d fish food with the name '%s' was found: %v", len(relevantFood), fishFood, relevantFood)
+				food, err := getFood(fishFood)
+				if err != nil {
+					return err
 				}
+				ohai.Ohaif("Uninstalling %s...\n", fishFood)
+				start := time.Now()
+				if err := food.Uninstall(); err != nil {
+					return err
+				}
+				t := time.Now()
+				ohai.Successf("%s %s: uninstalled in %s\n", food.Name, food.Version, t.Sub(start).String())
 			}
-			food, err := getFood(fishFood)
-			if err != nil {
-				return err
-			}
-			ohai.Ohaif("Uninstalling %s...\n", fishFood)
-			start := time.Now()
-			if err := food.Uninstall(); err != nil {
-				return err
-			}
-			t := time.Now()
-			ohai.Successf("%s %s: uninstalled in %s\n", food.Name, food.Version, t.Sub(start).String())
 			return nil
 		},
 	}
