8b4712fe1ee31f4bd3fef6da722366e266c7b43d
Yusuke Kuoka <ykuoka@gmail.com>
2020-08-16 09:18:31 +0900

    Create symlinks under $GOFISH_BINPATH
    Before this change, `gofish init` had been correctly using `$GOFISH_BINPATH` to override the default bin path(e.g. `/usr/local/bin` on *nix) to be created, but it was not using it when installing foods. This patch updates `gofish install` to create symlinks under `$GOFISH_BINPATH` if set, so that the behavior is consistent.

--- food.go
+++ food.go
@@ -246,7 +246,14 @@ func (f *Food) Link(pkg *Package) error {
 	barrelDir := filepath.Join(home.Barrel(), f.Name, f.Version)
 	for _, r := range pkg.Resources {
 		// TODO: run this in parallel
-		destPath := filepath.Join(home.HomePrefix, r.InstallPath)
+
+		// We assume every Food's InstallPath begins with `bin/` (`bin\\` on Windows)
+		installPath, err := filepath.Rel("bin", r.InstallPath)
+		if err != nil {
+			return err
+		}
+
+		destPath := filepath.Join(home.BinPath(), installPath)
 		if err := os.MkdirAll(filepath.Dir(destPath), 0755); err != nil && !os.IsExist(err) {
 			return err
 		}
