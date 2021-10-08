85258ae021f62b4d1c2022a3b5195e6ee873a023
stuart.warren <stuart.warren@ocado.com>
2021-10-08 13:56:26 +0100

    feat: basic auth credentials from netrc
    add basic auth credentials to request for food packages if present in users
    netrc file
    
    add 'Accept: application/octet-stream' header to permit working through [github
    api](https://docs.github.com/en/rest/reference/repos#get-a-release-asset)
    
    ref: #190

--- pkg/home/home_windows.go
+++ pkg/home/home_windows.go
@@ -9,3 +9,13 @@ const HomePrefix = "C:\\ProgramData"
 func Cache() string {
 	return userpath.Path("AppData", "Local", "gofish")
 }
+
+// GPGNetrc returns the path to an encrypted netrc file.
+func GPGNetrc() string {
+	return userpath.Path("_netrc.gpg")
+}
+
+// Netrc returns the path to a netrc file.
+func Netrc() string {
+	return userpath.Path("_netrc")
+}
